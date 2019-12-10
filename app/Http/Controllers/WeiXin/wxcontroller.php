86f574f6da3b096fc1f3f975065dbfd9dd3984c3
H0922 <737051678@qq.com>
2019-12-10 07:44:22 +0000

    链接微信接口

---
+++ app/Http/Controllers/WeiXin/wxcontroller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\WeiXin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class wxcontroller extends Controller
+{
+    public function wx(){
+        $token = '737051678ysd72bs7d2';
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $ec=$_GET['echostr'];
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        
+        if( $tmpStr == $signature ){
+            echo $ec;
+        }else{
+           die('not ok');
+        }
+    }
+}
