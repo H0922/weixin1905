c12313a9036de67989e7c42a79a0a3298d5acf7e
H0922 <737051678@qq.com>
2019-12-10 08:06:02 +0000

    微信日志

--- app/Http/Controllers/WeiXin/wxcontroller.php
+++ app/Http/Controllers/WeiXin/wxcontroller.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 
 class wxcontroller extends Controller
 {
+
+    //接收微信的推送事件
+    public function wxer(){
+        //将接收的数据记录存到日志文件
+        $log_file="wx.log";
+        $xml_str=file_get_contents("php://input");
+        $data=date('Y-m-d H:i:s',tiem()).$xml_str;
+        file_put_contents($log_file,$data,FILE_APPEND);
+    }
+
+
+    //链接微信接口
     public function wx(){
         $token = '737051678ysd72bs7d2';
         $signature = $_GET["signature"];
