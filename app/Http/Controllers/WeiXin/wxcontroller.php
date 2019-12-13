e453d4dc9dea1db09252fb3d7c85182d796a37e3
H0922 <737051678@qq.com>
2019-12-13 12:12:04 +0000

    整理代码

--- app/Http/Controllers/WeiXin/wxcontroller.php
+++ app/Http/Controllers/WeiXin/wxcontroller.php
@@ -6,14 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Model\WxUserModel as Mu;
 use Illuminate\Support\Facades\Redis;
+use GuzzleHttp\Client;
 class wxcontroller extends Controller
 {
     //储存access_token
     protected $access_token;
 
-    //魔术方法
-    public function __construct()
-    {
+        //魔术方法
+    public function __construct(){
         //给$access_token属性赋值
         $this->access_token=$this->getAccessToken();
     }
@@ -39,49 +39,47 @@ class wxcontroller extends Controller
         $log_file="wx.log";
         $xml_str=file_get_contents("php://input");
         $data=date('Y-m-d H:i:s',time()).$xml_str;
-        file_put_contents($log_file,$data,FILE_APPEND); 
-
-
+        file_put_contents($log_file,$data,FILE_APPEND);
+         //转换接收回来的数据 
+        $xml_obj=simplexml_load_string($xml_str);
+        $smg_type=$xml_obj->MsgType;
+        $MediaId=$xml_obj->MediaId;
+        $open_id=$xml_obj->FromUserName;
+        $touser=$xml_obj->ToUserName;
         //用户关注信息回复
         $this->subuser($xml_str);
-
-        //转换接收回来的数据
-        $xml_obj=simplexml_load_string($xml_str);
-        $img=$xml_obj->MsgType;
-       
-
-
-        //图片下载到本地
-        if($img=='image'){
-            $MediaId=$xml_obj->MediaId;
-            $open_id=$xml_obj->FromUserName;
-            $this->img($MediaId,$open_id);
-        }    
-
+        $this->UserSub($MediaId,$smg_type,$open_id,$touser);
     }
-        //图片下载到本地
-        public function img($MediaId,$open_id){
-            // echo $MediaId;
-            $url='https://api.weixin.qq.com/cgi-bin/media/get?access_token='.$this->access_token.'&media_id='.$MediaId.'';
-            $data=file_get_contents($url);
-            $user=$this->getUserInfo($open_id);
-            $op=$MediaId.'.jpg';
-            file_put_contents($op,$data);
+    //获取素材
+    public function getMedia($media_id,$smg_type){
+                $url ='https://api.weixin.qq.com/cgi-bin/media/get?access_token='.$this->access_token.'&media_id='.$media_id;
 
+                $client=new Client();
+                $response = $client->request('GET',$url);
+                // dd($response);
+                //获取文件类型
+                $content_type=$response->getHeader('Content-Type')[0];
+                $pos=strpos($content_type,'/');
+                $extension='.'.substr($content_type,$pos+1);
+                //获取文件内容
+                $file_con=$response->getBody();
+               
+                //保存文件
+                $save_path = 'wx_media/';
+                if($smg_type=='image'){
+                    $file_name = date('YmdHis').mt_rand(1111,9999).$extension;
+                    $save_path = $save_path.'imgs/'.$file_name;
+                }elseif($smg_type=='voice'){
+                    $file_name = date('YmdHis').mt_rand(1111,9999).$extension;
+                    $save_path = $save_path.'voice/'.$file_name;
+                }
+                file_put_contents($save_path,$file_con);
+                echo '创建成功';
+                // dd($save_path);
 
-            //创建文件夹
-            // $opfile='images/'.$user['nickname'].'/'.date('Y-m-d');
-            // $dir = iconv("UTF-8", "GBK", "$opfile");
-            //  if (!file_exists($dir)){
-            //         mkdir ($dir,0777,true);
-            //         echo '创建文件夹bookcover成功';
-            //     } else {
-            //            echo '需创建的文件夹bookcover已经存在';
-            //     }
-        }
-
+            }
        //用户关注
-       public function subuser($xml_str){
+    public function subuser($xml_str){
         //获取用户关注信息提示zss
         $xml_obj=simplexml_load_string($xml_str);
         $Event=$xml_obj->Event;
@@ -144,39 +142,39 @@ class wxcontroller extends Controller
             file_put_contents('wx_user.log',$data,FILE_APPEND);
         }
 
-        
-        //信息类型
-        $msg=$xml_obj->MsgType;
-        //纯文本信息回复
-        if($msg=='text'){
-        $con=date('Y-m-d H:i:s',time()).$xml_obj->Content;
+         }
+    //用户信息回复并保存
+    public function UserSub($MediaId,$msg,$from,$touser){
+        $time=time();
+        //图片信息回复
+        if($msg=='image'){
+            $this->getMedia($MediaId,$msg);
             $jie='<xml>
             <ToUserName><![CDATA['.$from.']]></ToUserName>
             <FromUserName><![CDATA['.$touser.']]></FromUserName>
             <CreateTime>'.$time.'</CreateTime>
-            <MsgType><![CDATA[text]]></MsgType>
-            <Content><![CDATA['.$con.']]></Content>
+            <MsgType><![CDATA[image]]></MsgType>
+            <Image>
+                <MediaId><![CDATA['.$MediaId.']]></MediaId>
+            </Image>
             </xml>';
             echo $jie;
         }
-        // //图片信息回复
-        // if($msg=='image'){
-        //     // $PicUrl=$xml_obj->PicUrl;
-        //     $MediaId=$xml_obj->MediaId;
-        //     $jie='<xml>
-        //     <ToUserName><![CDATA['.$from.']]></ToUserName>
-        //     <FromUserName><![CDATA['.$touser.']]></FromUserName>
-        //     <CreateTime>'.$time.'</CreateTime>
-        //     <MsgType><![CDATA[image]]></MsgType>
-        //     <Image>
-        //         <MediaId><![CDATA['.$MediaId.']]></MediaId>
-        //     </Image>
-        //     </xml>';
-        //     echo $jie;
-        // }
-        //语音
+         //纯文本信息回复
+         if($msg=='text'){
+            $con=date('Y-m-d H:i:s',time());
+                $jie='<xml>
+                <ToUserName><![CDATA['.$from.']]></ToUserName>
+                <FromUserName><![CDATA['.$touser.']]></FromUserName>
+                <CreateTime>'.$time.'</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA['.$con.']]></Content>
+                </xml>';
+                echo $jie;
+            }
+               //语音
         if($msg=='voice'){
-            $MediaId=$xml_obj->MediaId;
+            $this->getMedia($MediaId,$msg);
             $jie='<xml>
             <ToUserName><![CDATA['.$from.']]></ToUserName>
             <FromUserName><![CDATA['.$touser.']]></FromUserName>
@@ -188,9 +186,8 @@ class wxcontroller extends Controller
             </xml>';
             echo $jie;
         }
-        //  视频
-        if($msg=='video'){
-            $MediaId=$xml_obj->MediaId;
+         //  视频
+         if($msg=='video'){
             $title='公众号内测....haung';
             $desc='视频发布于'.date('Y-m-d H:i:s',time()).'huang';
             $jie='<xml>
@@ -204,12 +201,11 @@ class wxcontroller extends Controller
                 <Description><![CDATA['.$desc.']]></Description>
             </Video>
             </xml>';
-            return $jie;
+            echo $jie;
         }
     }
-
-         //获取用户的基本信息
-         public function getUserInfo($open_id){
+    //获取用户的基本信息
+    public function getUserInfo($open_id){
             $url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->getAccessToken().'&openid='.$open_id.'&lang=zh_CN';
             //发送网络请求   发送的get的请求
             $json_str=file_get_contents($url);
@@ -217,7 +213,7 @@ class wxcontroller extends Controller
             return $data;
     
         }
-
+        
 
     //链接微信接口
     public function wx(){
