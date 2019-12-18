a594be653627ab014ef04777bdfdd9aaee4b4ae2
H0922 <737051678@qq.com>
2019-12-18 05:51:24 +0000

    ceshi

--- app/Http/Controllers/WeiXin/VoteConteller.php
+++ app/Http/Controllers/WeiXin/VoteConteller.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\WeiXin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 use function GuzzleHttp\json_decode;
 use function GuzzleHttp\json_encode;
-
+use Illuminate\Support\Facades\Redis;
 class VoteConteller extends Controller
 {
     
@@ -23,9 +24,15 @@ class VoteConteller extends Controller
        $access_tokrn=$token['access_token'];
        $openid=$token['openid'];
        $user=$this->Userxi($access_tokrn,$openid);
-       
+       //展示
+       $this->list($user);
    }
-
+         //展示
+        public function list($user){
+            dump($user);
+            $number=Redis::incr('vote');
+            echo "投票成功，当前票数".$number;
+        }
    //获取Token
    public function AccessToken($code){
         $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
