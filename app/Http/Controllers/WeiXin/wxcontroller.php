a804ee05e06f1f787c8ac9d340f0fb12f6c5a9bf
H0922 <737051678@qq.com>
2019-12-19 14:01:34 +0000

    ceshi

--- app/Http/Controllers/WeiXin/wxcontroller.php
+++ app/Http/Controllers/WeiXin/wxcontroller.php
@@ -298,7 +298,7 @@ class wxcontroller extends Controller
         $ewd_url=urlencode($urll);
         $goods='http://www.bianaoao.top/goods/goods';
         $goods_url=urlencode($goods);
-
+        dd('https://open.weixin.qq.com/connect/oauth2/authorize?appid='.env('WX_APPID').'&redirect_uri='.$goods_url.'&response_type=code&scope=snsapi_userinfo&state=1905goods#wechat_redirect');
         $url='https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.$this->access_token;
         $menu=[
             'button'=>[
