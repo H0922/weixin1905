ace549ed132fbf0e9fea1268459821c676e08fc9
H0922 <737051678@qq.com>
2019-12-26 03:33:52 +0000

    kecheng

--- resources/views/weixin/wx/upd.blade.php
+++ resources/views/weixin/wx/upd.blade.php
@@ -15,40 +15,40 @@
                 <td>第一节课：</td>
                 <td><select name="ka" id="">
                         <option value="">--请选择--</option>    
-                        <option value="{{$link->ka=='php' ? 'selected' : ''}}">php</option>    
-                        <option value="{{$link->ka=='语文' ? 'selected' : ''}}">语文</option>    
-                        <option value="{{$link->ka=='数学' ? 'selected' : ''}}">数学</option>    
-                        <option value="{{$link->ka=='英语' ? 'selected' : ''}}">英语</option>    
+                        <option value="php" {{$link->ka=='php' ? 'selected' : ''}}>php</option>    
+                        <option value="语文" {{$link->ka=='语文' ? 'selected' : ''}}>语文</option>    
+                        <option value="数学" {{$link->ka=='数学' ? 'selected' : ''}}>数学</option>    
+                        <option value="英语" {{$link->ka=='英语' ? 'selected' : ''}}>英语</option>    
                 </select></td>
             </tr>
             <tr>
                 <td>第二节课：</td>
                 <td><select name="kb" id="">
                         <option value="">--请选择--</option>    
-                        <option value="{{$link->kb=='php' ? 'selected' : ''}}">php</option>    
-                        <option value="{{$link->kb=='语文' ? 'selected' : ''}}">语文</option>    
-                        <option value="{{$link->kb=='数学' ? 'selected' : ''}}">数学</option>    
-                        <option value="{{$link->kb=='英语' ? 'selected' : ''}}">英语</option> 
+                        <option value="php" {{$link->kb=='php' ? 'selected' : ''}}>php</option>    
+                        <option value="语文" {{$link->kb=='语文' ? 'selected' : ''}}>语文</option>    
+                        <option value="数学" {{$link->kb=='数学' ? 'selected' : ''}}>数学</option>    
+                        <option value="英语" {{$link->kb=='英语' ? 'selected' : ''}}>英语</option>    
                 </select></td>
             </tr>
             <tr>
                 <td>第三节课：</td>
                 <td><select name="kc" id="">
                         <option value="">--请选择--</option>    
-                        <option value="{{$link->kc=='php' ? 'selected' : ''}}">php</option>    
-                        <option value="{{$link->kc=='语文' ? 'selected' : ''}}">语文</option>    
-                        <option value="{{$link->kc=='数学' ? 'selected' : ''}}">数学</option>    
-                        <option value="{{$link->kc=='英语' ? 'selected' : ''}}">英语</option> 
+                        <option value="php" {{$link->kc=='php' ? 'selected' : ''}}>php</option>    
+                        <option value="语文" {{$link->kc=='语文' ? 'selected' : ''}}>语文</option>    
+                        <option value="数学" {{$link->kc=='数学' ? 'selected' : ''}}>数学</option>    
+                        <option value="英语" {{$link->kc=='英语' ? 'selected' : ''}}>英语</option>    
                 </select></td>
             </tr>
             <tr>
                 <td>第四节课：</td>
                 <td><select name="kd" id="">
                         <option value="">--请选择--</option>    
-                        <option value="{{$link->kd=='php' ? 'selected' : ''}}">php</option>    
-                        <option value="{{$link->kd=='语文' ? 'selected' : ''}}">语文</option>    
-                        <option value="{{$link->kd=='数学' ? 'selected' : ''}}">数学</option>    
-                        <option value="{{$link->kd=='英语' ? 'selected' : ''}}">英语</option> 
+                        <option value="php" {{$link->kd=='php' ? 'selected' : ''}}>php</option>    
+                        <option value="语文" {{$link->kd=='语文' ? 'selected' : ''}}>语文</option>    
+                        <option value="数学" {{$link->kd=='数学' ? 'selected' : ''}}>数学</option>    
+                        <option value="英语" {{$link->kd=='英语' ? 'selected' : ''}}>英语</option>    
                 </select></td>
             </tr>
             <tr>
